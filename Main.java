86beeed3308b5c86eb9a081a803966284989fae3
Alex Frolov <98168002+PieceOfBulka@users.noreply.github.com>
2024-06-09 19:50:05 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,8 +2,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 
 public class Main {
+    
+     //Понадобятся при тестах
+    Random r=new Random();
+    long time;
+    
     public static ArrayList<Long> ReadFile(String path) {
         Scanner scanner;
         try {
@@ -47,6 +54,90 @@ public class Main {
         return mlt;
     }
 
+
+    
+    //Тесты для проверки скорости работы программы при увеличении размера входного файла
+
+    void minTimeTest() {
+        ArrayList<Long> arr=new ArrayList<>();
+        int size=1000;
+        for (int j=1;j<6;j++) {
+            for (int i = 0; i < size*(int)Math.pow(10,j); i++) {
+                arr.add(r.nextLong(1000000000));
+            }
+            time = System.currentTimeMillis();
+            min(arr);
+            System.out.println("Скорость работы при размере "+arr.size()+" элементов = "+(System.currentTimeMillis() - time)+" мс");
+        }
+    }
+
+    void maxTimeTest() {
+        ArrayList<Long> arr=new ArrayList<>();
+        int size=1000;
+        for (int j=1;j<6;j++) {
+            for (int i = 0; i < size*(int)Math.pow(10,j); i++) {
+                arr.add(r.nextLong(1000000000));
+            }
+            time = System.currentTimeMillis();
+            max(arr);
+            System.out.println("Скорость работы при размере "+arr.size()+" элементов = "+(System.currentTimeMillis() - time)+" мс");
+        }
+    }
+
+    void sumTimeTest() {
+        ArrayList<Long> arr=new ArrayList<>();
+        int size=1000;
+        for (int j=1;j<6;j++) {
+            for (int i = 0; i < size*(int)Math.pow(10,j); i++) {
+                arr.add(r.nextLong(1000000000));
+            }
+            time = System.currentTimeMillis();
+            sum(arr);
+            System.out.println("Скорость работы при размере "+arr.size()+" элементов = "+(System.currentTimeMillis() - time)+" мс");
+        }
+    }
+
+    void multTimeTest() {
+        ArrayList<Long> arr=new ArrayList<>();
+        int size=1000;
+        for (int j=1;j<6;j++) {
+            for (int i = 0; i < size*(int)Math.pow(10,j); i++) {
+                arr.add(r.nextLong(1000000000));
+            }
+            time = System.currentTimeMillis();
+            mult(arr);
+            System.out.println("Скорость работы при размере "+arr.size()+" элементов = "+(System.currentTimeMillis() - time)+" мс");
+        }
+    }
+
+
+
+    //Тесты для проверки времени процессорного исполнения программы
+
+    //Метод для подсчета времени процессора
+    private static long getCpuTime() {
+        ThreadMXBean bean = ManagementFactory.getThreadMXBean();
+        return bean.isCurrentThreadCpuTimeSupported() ? bean.getCurrentThreadCpuTime() : 0L;
+    }
+
+
+    void minCPUTest(){
+        ArrayList<Long> arr=new ArrayList<>();
+        int rnd=r.nextInt(10000000);
+        long actual=r.nextLong();
+        arr.add(actual);
+        for (int i=0;i<rnd;i++){
+            arr.add(r.nextLong(1000000000));
+            actual*=arr.getLast();
+        }
+        long startCPUTime = getCpuTime();
+        min(arr);
+        long endCPUTime = getCpuTime();
+
+        long cpuTime = endCPUTime - startCPUTime;
+        System.out.println("Процессорное время: " + cpuTime + " наносекунд");
+    }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Введите путь до файла: ");
@@ -58,5 +149,12 @@ public class Main {
         System.out.println("Максимальное: "+ max(list));
         System.out.println("Сумма: "+ sum(list));
         System.out.println("Произведение: "+ mult(list));
+        System.out.println();
+        System.out.println("Тест функции минимума:\n", minTimeTest());
+        System.out.println("Тест функции максимума:\n", maxTimeTest());
+        System.out.println("Тест функции суммы:\n", sumTimeTest());
+        System.out.println("Тест функции произведения:\n", multTimeTest());
+        System.out.println();
+        System.out.println("Тест функции минимума:\n", minCPUTest());
     }
 }
